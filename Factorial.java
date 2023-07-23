8ad2227359d3a06ee927e6c5872f83b6925ddbd7
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2023-07-23 13:13:01 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,29 @@
+// public class Factorial {
+//     public static void main(String [] args){
+        
+//         int no=6;
+//         int fact=1;
+//         // for(int i=1;i<=no;i++){
+//         //     fact=fact*i;
+//         // }
+
+
+//         // 
+//         for(int i=no;i>=1;i--){
+//             fact=fact*i;
+//         }
+//         System.out.println(fact);
+//     }
+// }
+
+public class Factorial{
+    public static void main(String[] args){
+        int no=7;
+        int fact=1;
+        for(int i=1;i<=no;i++){
+            
+                fact=fact*i;
+            }
+            System.out.println(fact);
+        }
+    }
