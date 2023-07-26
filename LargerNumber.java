84c2f969342df52d23906972698b4cdabb55b9e7
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2023-07-26 09:17:31 +0530

    Add files via upload

---
+++ LargerNumber.java
@@ -0,0 +1,12 @@
+public class LargerNumber {
+    public static void main(String[]args){
+        int a=10;
+        int b=20;
+        if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+    }
+}
