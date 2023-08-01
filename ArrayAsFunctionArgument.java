dedf215822f9019294b2ad6b298ea1aca26cc3ab
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2023-08-01 10:48:40 +0530

    Add files via upload

---
+++ ArrayAsFunctionArgument.java
@@ -0,0 +1,16 @@
+public class ArrayAsFunctionArgument {
+    public static void update(int marks[]){
+        for(int i=0;i<marks.length;i++){
+            marks[i]=marks[i]+1;
+        }
+    }
+    public static void main(String args[]){
+        int marks[]={97,98,80};
+        update(marks);
+
+        for(int i=0;i<marks.length;i++){
+            System.out.print(marks[i]+" ");
+        }
+        System.out.println();
+    }
+}
