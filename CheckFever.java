386133c3de6dde203295012ffdf94e23bf592b72
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2023-08-06 23:16:33 +0530

    Add files via upload

---
+++ CheckFever.java
@@ -0,0 +1,11 @@
+public class CheckFever {
+    public static void main(String[] args){
+        double temp=103.5;
+        if(temp>100){
+            System.out.println("you have fever");
+        }
+        else{
+            System.out.println("you have not a fever");
+        }
+    }
+}
