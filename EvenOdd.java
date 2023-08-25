83f351d731d37d722ff73c5cf234924de81af960
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2023-08-25 21:10:11 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+    public static void main(String[] args){
+        int a=33;
+        if(a%2==0){
+            System.out.println("even no.");
+        }
+        else{
+            System.out.println("odd no.");
+        }
+    }
+}
